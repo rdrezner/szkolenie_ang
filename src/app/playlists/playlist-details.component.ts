f275f3d77a298aa19b024fa1757b2ee7fd0a45ca
Mateusz Kulesza <ev45ive@gmail.com>
2017-10-09 15:38:56 +0200

    Hidden

--- src/app/playlists/playlist-details.component.ts
+++ src/app/playlists/playlist-details.component.ts
@@ -4,37 +4,35 @@ import { Playlist } from './playlist';
 @Component({
   selector: 'playlist-details',
   template: `
-  <div>
-  <div class="form-group">
-      <label>Name: </label>
-      <div class="form-control-static"> {{playlist.name}} </div>
-  </div>
-  <div class="form-group">
-      <label>Favourite: </label>
-      <div class="form-control-static"> {{ playlist.favourite? 'Favouite' : 'Nope' }} </div>
-  </div>
-  <div class="form-group">
-      <label>Color: </label>
-      <div class="form-control-static" 
-        [style.backgroundColor]="playlist.color"> {{ playlist.color }} </div>
-  </div>
+  <div [hidden]="mode != 'show'">
+    <div class="form-group">
+        <label>Name: </label>
+        <div class="form-control-static"> {{playlist.name}} </div>
+    </div>
+    <div class="form-group">
+        <label>Favourite: </label>
+        <div class="form-control-static"> {{ playlist.favourite? 'Favouite' : 'Nope' }} </div>
+    </div>
+    <div class="form-group">
+        <label>Color: </label>
+        <div class="form-control-static" 
+          [style.backgroundColor]="playlist.color"> {{ playlist.color }} </div>
+    </div>
+    <button class="btn btn-success float-right" (click)="edit()">Edit</button>
   </div>
 
-  <div>
+  <div [hidden]="mode != 'edit'">
     <div class="form-group">
         <label>Name</label>
-        <input type="text" class="form-control" [ngModel]="playlist.name"
-                              (ngModelChange)="playlist.name = $event">
+        <input type="text" class="form-control" [(ngModel)]="playlist.name">
     </div>
     <div class="form-group">
         <label>Favourite</label>
-        <input type="checkbox" [ngModel]="playlist.favourite"
-                          (ngModelChange)="playlist.favourite = $event">
+        <input type="checkbox" [(ngModel)]="playlist.favourite">
     </div>
     <div class="form-group">
         <label>Color</label>
-        <input type="color" [ngModel]="playlist.color"
-                          (ngModelChange)="playlist.color = $event">
+        <input type="color" [(ngModel)]="playlist.color">
     </div>
     <button class="btn btn-success float-right" (click)="save()">Save</button>
     <button class="btn btn-danger float-right" (click)="reset()">Cancel</button>
@@ -51,12 +49,18 @@ export class PlaylistDetailsComponent implements OnInit {
     color: "#00ff00",
   }
 
+  mode = 'show'
+
+  edit() {
+    this.mode = 'edit'
+  }
+
   save() {
     console.log('save')
   }
 
   reset() {
-    console.log('reset')
+    this.mode = 'show'
   }
 
   constructor() { }
