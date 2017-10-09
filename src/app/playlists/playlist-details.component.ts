25b264f21bb2d1a9e5dd68d25447d5cd63a8d579
Mateusz Kulesza <ev45ive@gmail.com>
2017-10-09 15:14:50 +0200

    2-way binding

--- src/app/playlists/playlist-details.component.ts
+++ src/app/playlists/playlist-details.component.ts
@@ -23,27 +23,40 @@ import { Playlist } from './playlist';
   <div>
     <div class="form-group">
         <label>Name</label>
-        <input type="text" class="form-control" [value]="playlist.name">
+        <input type="text" class="form-control" [value]="playlist.name"
+                              (input)="playlist.name = $event.target.value">
     </div>
     <div class="form-group">
         <label>Favourite</label>
-        <input type="checkbox" [checked]="playlist.favourite">
+        <input type="checkbox" [checked]="playlist.favourite"
+                          (change)="playlist.favourite = $event.target.checked">
     </div>
     <div class="form-group">
         <label>Color</label>
-        <input type="color" [value]="playlist.color">
+        <input type="color" [value]="playlist.color"
+                          (change)="playlist.color = $event.target.value">
     </div>
+    <button class="btn btn-success float-right" (click)="save()">Save</button>
+    <button class="btn btn-danger float-right" (click)="reset()">Cancel</button>
   </div>
   `,
   styles: []
 })
 export class PlaylistDetailsComponent implements OnInit {
 
-  playlist:Playlist = {
+  playlist: Playlist = {
     id: 123,
     name: "Angular greatest Hits",
-    favourite:false,
+    favourite: false,
     color: "#00ff00",
+  }
+
+  save() {
+    console.log('save')
+  }
+
+  reset() {
+    console.log('reset')
   }
 
   constructor() { }
